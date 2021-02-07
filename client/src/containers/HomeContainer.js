08f2445a1b91f9284b55c5eb275930dfef922988
Pradeep Suresh <pradeepsuresh@Pradeeps-MacBook-Pro-2.local>
2021-02-07 01:47:38 -0800

    Modified the OverviewPage

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './HomeContainer.css'
-import StockTable from './StockTableContainer'
+import AppBar from './../components/AppBar'
 
 function HomeContainer () {
     return (
-        <div className = 'home-container'>
-            <div className = 'navigation-bar'>
-               <div className = 'title'>
-                   Rookie Investor
-               </div>
-               <div className = 'menu-bar'>
-                   Menu Bar
-               </div>
-            </div> 
-            <div className = 'stock-table'>
-                <StockTable />
-            </div> 
-            <div className = 'footer'>
-                footer
-            </div> 
+        <div className='home-container'>
+                <AppBar />
+            <div className='dashboard'>
+                dashboard
+            </div>
+    
+
         </div>
     )
 }
